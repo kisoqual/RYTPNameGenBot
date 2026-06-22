74a3fb4543fb9592be2fa938cb850c06b0273d7c
kisoqual <64285271+kisoqual@users.noreply.github.com>
2026-06-22 13:09:11 +0300

    Update script.js

--- namegen/script.js
+++ namegen/script.js
@@ -2,8 +2,9 @@
     const cBtn = document.querySelector('#copyBtn')
     j = prompt("какой ты RYTP хочеш? ");
     document.write(`<h1>#ники@futurepooqs #название@futurepooqs<br>${j} ${i}</h1>`);
+    let result = `#ники@futurepooqs #название@futurepooqs\n${j} ${i}`
     cBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(`#ники@futurepooqs #название@futurepooqs\n${j} ${i}`)
+        navigator.clipboard.writeText(result)
         alert('Скопировано!')
         //cBtn.innerHTML = "<button id="copyBtn">Скопировано!</button>"
     })
