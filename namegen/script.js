d07f3a17e5e674fdbe9fd61fb7ecad50187fe8f2
kisoqual <64285271+kisoqual@users.noreply.github.com>
2026-06-22 12:58:40 +0300

    Update script.js

--- namegen/script.js
+++ namegen/script.js
@@ -2,7 +2,7 @@ const cBtn = document.querySelector('#copyBtn')
 function lul(i, j) {
     j = prompt("какой ты RYTP хочеш? ");
     document.write(`
-        <h1>#ники@futurepooqs #название@futurepooqs<br>" ${j} ${i} "</h1>
+        <h1>#ники@futurepooqs #название@futurepooqs<br>${j} ${i}</h1>
             <h1>
         <a href="/RYTPNameGenBot">
             ← Назад в меню
@@ -11,7 +11,7 @@ function lul(i, j) {
     </h1>
     `);
     cBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText("#ники@futurepooqs #название@futurepooqs ", j, i)
+        navigator.clipboard.writeText(`#ники@futurepooqs #название@futurepooqs\n${j} ${i}`)
         alert('Скопировано!')
         //cBtn.innerHTML = "<button id="copyBtn">Скопировано!</button>"
     })
