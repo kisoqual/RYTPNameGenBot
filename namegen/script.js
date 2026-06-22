fd0762ad0204fe8de81adca76f2c3baba7266877
kisoqual <64285271+kisoqual@users.noreply.github.com>
2026-06-22 13:37:15 +0300

    Update script.js

--- namegen/script.js
+++ namegen/script.js
@@ -3,4 +3,5 @@
     const content = `<h1>#ники@futurepooqs #название@futurepooqs<br>${j} ${i}</h1>`
     const outputContainer = document.createElement('div')
     outputContainer.innerHTML = content
+    document.body.append(outputContainer)
 })(" RYTP", "")
